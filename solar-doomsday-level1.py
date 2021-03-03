a509929e2c0355dc94720738c0b4eeb5a3cc9f8b
Alisher <alisher@zonghanwus-MacBook-Air.local>
2021-03-03 23:03:25 +0800

    [First level finished]

---
+++ solar-doomsday-level1.py
@@ -0,0 +1,18 @@
+def solution(area):
+    result = []
+
+    def recursion(area):
+        if area == 0:
+            return
+        if area < 2:
+            result.append(1)
+            return
+        
+        x = area // 2
+        while x * x > area:
+            x = (x + area // x) // 2
+        result.append(x * x)
+        recursion(area - (x * x))
+
+    recursion(area)
+solution(16)
